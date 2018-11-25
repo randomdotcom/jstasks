571176e41ea2eaee888e3860aea19dc78a8fdb00
randomdotcom <43496357+randomdotcom@users.noreply.github.com>
2018-11-25 15:45:41 +0300

    Add files via upload
    datepicker

--- js_calendar/script.js
+++ js_calendar/script.js
@@ -109,6 +109,7 @@ for (var i = 1; i <= 42; i++) {
     let numberBlock = document.createElement("div");
     monthsDaysBlock.appendChild(numberBlock);
     numberBlock.classList.add("numberBlock");
+    numberBlock.addEventListener("click", showPickedDate);
 
     let number = document.createElement("p");
     numberBlock.appendChild(number);
@@ -130,8 +131,110 @@ for (var i = 1; i <= 42; i++) {
 }
 
 
+////////////// Date Picker
+var datePickerBlock = document.createElement("div");
+main.appendChild(datePickerBlock);
+datePickerBlock.classList.add("datePickerBlock");
+
+var inputDay = document.createElement("input");
+datePickerBlock.appendChild(inputDay);
+inputDay.classList.add("input");
+inputDay.type = "number";
+inputDay.value = curDay;
+inputDay.addEventListener("input", onChangeDay);
 
 
+var inputMonth = document.createElement("input");
+datePickerBlock.appendChild(inputMonth);
+inputMonth.classList.add("input");
+inputMonth.type = "number";
+inputMonth.value = curMonth+1;
+inputMonth.addEventListener("input", onChangeMonth);
+
+var inputYear = document.createElement("input");
+datePickerBlock.appendChild(inputYear);
+inputYear.classList.add("input");
+inputYear.type = "number";
+inputYear.value = curYear;
+inputYear.addEventListener("input", onChangeYear);
+
+var goToDate = document.createElement("div");
+datePickerBlock.appendChild(goToDate);
+goToDate.classList.add("goToDate");
+goToDate.addEventListener("click", goToPickedDate);
+
+var goToDateText = document.createElement("p");
+goToDate.appendChild(goToDateText);
+goToDateText.classList.add("goToDateText");
+goToDateText.innerText = "go";
+
+
+function showPickedDate(event) {
+    if (event.target.innerText) {
+    inputDay.value = event.target.innerText;
+    inputMonth.value = month+1;
+    inputYear.value = year;
+    }
+}
+function goToPickedDate() {
+    month = +inputMonth.value-1;
+    year = +inputYear.value;
+
+    pickedMonth.innerText = getTextMonth(month) + " " + year;
+
+    clearMonth();
+
+
+    let todayDayBlock = document.getElementsByClassName("today")[0];
+    if (curMonth == month && curYear == year) {
+        todayDayBlock.style.backgroundColor = "#3d3d3d";
+        todayDayBlock.style.color = "#a4d337";
+    } else {
+        todayDayBlock.style.backgroundColor = "transparent";
+        todayDayBlock.style.color = "black";
+    }
+
+
+    for (var i = 1; i <= 42; i++) {
+        let d = new Date(year, month, 1);
+        d = d.getDay();
+        d = (d === 0) ? 7 : d;
+
+        if (i >= d) {
+            if (i - d + 1 <= daysInMonth(month, year) && i - d + 1 >= 1) number[i - 1].innerText = i - d + 1;
+        }
+        if (number[i-1].innerText == inputDay.value) {
+            
+        }
+    }
+}
+
+function onChangeDay() {
+    if (inputDay.value > daysInMonth(inputMonth.value-1, inputYear.value)) {
+        inputMonth.value = +inputMonth.value + 1;
+        inputDay.value = 1;
+    }
+    if (inputDay.value < 1) {
+        inputMonth.value = +inputMonth.value - 1;
+        inputDay.value = daysInMonth(inputMonth.value-1, inputYear.value);
+    } 
+}
+
+function onChangeMonth() {
+    if (inputMonth.value > 12) {
+        inputYear.value = +inputYear.value + 1;
+        inputMonth.value = 1;
+    }
+    else if (inputMonth.value < 1) {
+        inputYear.value = +inputYear.value - 1;
+        inputMonth.value = 12; 
+    }
+    if (inputDay.value > daysInMonth(inputMonth.value-1, inputYear.value)) inputDay.value = daysInMonth(inputMonth.value-1, inputYear.value);
+}
+
+function onChangeYear() {
+    if (inputDay.value > daysInMonth(inputMonth.value - 1, inputYear.value)) inputDay.value = daysInMonth(inputMonth.value - 1, inputYear.value);
+}
 
 function nextMonth() {
 
@@ -147,7 +250,7 @@ function nextMonth() {
 
     
     let todayDayBlock = document.getElementsByClassName("today")[0];
-    if (curMonth==month) {
+    if (curMonth==month && curYear == year) {
         todayDayBlock.style.backgroundColor = "#3d3d3d";
         todayDayBlock.style.color = "#a4d337";
     } else {
@@ -178,7 +281,7 @@ function prevMonth() {
 
 
     let todayDayBlock = document.getElementsByClassName("today")[0];
-    if (curMonth == month) {
+    if (curMonth == month && curYear == year) {
         todayDayBlock.style.backgroundColor = "#3d3d3d";
         todayDayBlock.style.color = "#a4d337";
     } else {
@@ -230,3 +333,5 @@ function getTextMonth(Month) {
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
 }
+
+function isNum(n) { return Number(n) === n; }
